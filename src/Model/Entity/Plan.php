72fc59666bb56e7d4a192c94b177eaace3165686
Unknown <vincent.fresnel@gmail.com>
2019-07-18 11:51:53 +0200

    add stack option
    add stack option

--- src/Model/Entity/Plan.php
+++ src/Model/Entity/Plan.php
@@ -359,8 +359,8 @@ class Plan implements EntityInterface
                     $background_color = '';
                 }
                 $html = '<div class="graph-widget" data-graph_id="' . $this->getLink_id() . '" style="' . $background_color . 'border : solid 1px black;min-height:50px;min-width:50px;">';
-                $html .= '<span class="graphOptions" style="display:none;">' . json_encode($this->getDisplay(PlanDisplayType::GRAPH, array())) . '</span>';
-                $html .= '<div class="graph" id="graph' . $this->getLink_id() . '" style="width : 100%;height : 100%;"></div>';
+                $html .= '<span class="graphOptions" style="display:none;">' . json_encode($this->getDisplay('graph', array())) . '</span>';
+                $html .= '<canvas class="graph" id="graph' . $this->getLink_id() . '" style="width : 100%;height : 100%;"></canvas>';
                 $html .= '</div>';
                 return array(
                     'plan' => Utils::o2a($this),
